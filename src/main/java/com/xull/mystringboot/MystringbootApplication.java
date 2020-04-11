75462ca9ddda98476b67153031dadc5126b4cbdc
xull_ling <xull_ling@163.com>
2020-04-11 18:58:50 +0800

    MyBatis Generator 集成

--- src/main/java/com/xull/mystringboot/MystringbootApplication.java
+++ src/main/java/com/xull/mystringboot/MystringbootApplication.java
@@ -1,9 +1,11 @@
 package com.xull.mystringboot;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.xull.mystringboot.mapper")
 public class MystringbootApplication {
 
     public static void main(String[] args) {
