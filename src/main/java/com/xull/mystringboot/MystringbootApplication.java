f4af0dc3ba08815adad054c8b07efa0d23e1d034
xull_ling <xull_ling@163.com>
2020-04-11 14:56:27 +0800

    init

---
+++ src/main/java/com/xull/mystringboot/MystringbootApplication.java
@@ -0,0 +1,13 @@
+package com.xull.mystringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MystringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MystringbootApplication.class, args);
+    }
+
+}
