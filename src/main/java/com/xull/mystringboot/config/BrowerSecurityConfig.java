b45537e674b6ece880156c48c9a5d576e8f1bec4
xululu088 <284904817@qq.com>
2020-07-11 19:13:18 +0800

    spring security集成

---
+++ src/main/java/com/xull/mystringboot/config/BrowerSecurityConfig.java
@@ -0,0 +1,31 @@
+package com.xull.mystringboot.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class BrowerSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    MyAuthenctiationSuccessHandler myAuthenctiationSuccessHandler;
+    @Autowired
+    MyAuthenctiationFailureHandler myAuthenctiationFailureHandler;
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin()                    //  定义当需要用户登录时候，转到的登录页面。
+                .successHandler(myAuthenctiationSuccessHandler)
+                .failureHandler(myAuthenctiationFailureHandler)
+                .and()
+                .authorizeRequests()        // 定义哪些URL需要被保护、哪些不需要被保护
+                .anyRequest()               // 任何请求,登录后可以访问
+                .authenticated();
+    }
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
